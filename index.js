23cb57ebcf4a3c3f7a83a15bf5c0f0315cefd487
Iliyan Yordanov <63467086+ilian4o96@users.noreply.github.com>
2022-12-17 13:57:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
+
+//crear la app
+const app = express()
+
+//conexión a la base de datos
+const conectarDB = async () => {
+try {
+    await db.authenticate();
+    console.log('Conexión correcta a la base de datos')
+} catch (error){
+    console.log(error)
+}
+}
+conectarDB()
+//Habilitar pug
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+//Carpeta Publica
+app.use(express.static('public'))
+
+//routing
+app.use('/auth',usuarioRoutes)
+
+
+
+//definir un puerto y arrancar el servidor
+const port = 3000;
+app.listen(port, ()=> {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
